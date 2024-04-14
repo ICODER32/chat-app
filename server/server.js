6f7a885b793ab974eefc29a653105761efad0cd5
Ibtisam Anwar <ibtisamanwar008@gmail.com>
2024-04-14 15:52:52 +0500

    port defined

--- server/server.js
+++ server/server.js
@@ -2,10 +2,11 @@ import express from "express";
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
