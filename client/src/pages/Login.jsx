b1c08ff27424e0f8f113e6f712dd361cfce5da4a
Ibtisam Anwar <ibtisamanwar008@gmail.com>
2024-04-26 15:42:39 +0500

    frontend added

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+export default function Login() {
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className=" w-full p-6 bg-gray-400 rounded-full shadow-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+        <h1 className=" text-3xl font-semibold text-center text-gray-300">
+          Login
+        </h1>
+      </div>
+    </div>
+  );
+}
